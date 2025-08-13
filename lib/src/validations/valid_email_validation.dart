d08361004959e1d623c4aedf106d3d9c81fe8f77
Rogério M. Queiroz <rogerio.mq@gmail.com>
2025-08-13 20:21:01 -0300

    fix: improve email validation regex and enhance test coverage

--- lib/src/validations/valid_email_validation.dart
+++ lib/src/validations/valid_email_validation.dart
@@ -24,7 +24,8 @@ extension ValidEmailValidation on SimpleValidationBuilder<String> {
   ///
   SimpleValidationBuilder<String> validEmail({String? message, String? code}) {
     return use((value, entity) {
-      if (RegExp(r'^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$').hasMatch(value)) {
+      if (RegExp(r'^[\w\.-]+@([A-Za-z0-9][A-Za-z0-9\-]*\.)+[A-Za-z]{2,}$')
+          .hasMatch(value)) {
         return null;
       }
 
--- test/src/validations/valid_email_validation_test.dart
+++ test/src/validations/valid_email_validation_test.dart
@@ -31,10 +31,30 @@ void main() {
         .ruleFor((e) => e.email, key: 'email', label: 'E-mail') //
         .validEmailOrNull();
 
-    final user = UserNullableModel()..email =  null;
+    final user = UserNullableModel()..email = null;
 
     final result = validator.validate(user);
 
     expect(result.isValid, true);
+  });
+
+  test('valid email validation...', () {
+    final validator = TestLucidValidator<UserModel>();
+
+    validator
+        .ruleFor((e) => e.email, key: 'email', label: 'E-mail') //
+        .validEmail();
+
+    final user = UserModel()..email = 'test@.123.com';
+
+    final result = validator.validate(user);
+
+    expect(result.isValid, false);
+
+    expect(result.exceptions.length, 1);
+
+    final error = result.exceptions.first;
+
+    expect(error.message, "'E-mail' is not a valid email address.");
   });
 }
